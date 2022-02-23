a4987bf4e048db83f93a45f48ed0af03b08fea9e
khushbu vaishnani <khushbu@formics.io>
2022-02-23 17:42:07 +0530

    add-employee screen design

--- src/app/my-components/add-employee/add-employee.component.ts
+++ src/app/my-components/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUserTie } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-employee',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee.component.scss']
 })
 export class AddEmployeeComponent implements OnInit {
+
+  avtar = faUserTie;
 
   constructor() { }
 
