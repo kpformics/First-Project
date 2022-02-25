d1f075195a0b04a71964998a60c70aa495a657cc
khushbu vaishnani <khushbu@formics.io>
2022-02-25 19:07:36 +0530

    add employee screen work start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +30,7 @@ import { EditEmployeeComponent } from './my-components/edit-employee/edit-employ
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     FontAwesomeModule,
     AppRoutingModule,
     NgbModule,
--- src/app/my-components/add-employee/add-employee.component.ts
+++ src/app/my-components/add-employee/add-employee.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, KeyValueChanges, OnInit } from '@angular/core';
 import { faUserTie } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-add-employee',
@@ -8,11 +9,33 @@ import { faUserTie } from '@fortawesome/free-solid-svg-icons';
 })
 export class AddEmployeeComponent implements OnInit {
 
-  avtar = faUserTie;
+  avtar = faUserTie;//icon
+
+data:any = {
+  name:"",
+  birthdate:"",
+  gender:"",
+  city:"",
+  email:"",
+  phone:"",
+  jobtitle:"",
+  emptype:"",
+  joiningdate:"",
+  contractexpiry:"",
+  doc:"",
+  salary:""
+}
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  dosubmitform() {
+    console.log("try to submit form");
+    console.log("DATA", this.data);
   }
 
 }
